e74d3ca17193126c12f12725c5655f9726ce6016
Puneet Bawa <bawapuneet994@gmail.com>
2019-11-19 09:45:29 +0530

    Adding basics of R programming including assignment statements and basic arithmetic operations

---
+++ introduction/assign_operations.R
@@ -0,0 +1,18 @@
+
+#assigning numeric value to the variable
+my_numeric <- 42
+
+#assigning character value to the variable
+my_character <- "universe"
+
+#assigning boolean value to the variable "TRUE or FALSE"
+my_logical <- FALSE
+
+
+#INORDER TO VERIFY THE VARIABLE DATATYPE, the function class(_var_name_) is used
+
+class(my_numeric) #Output will be "numeric"
+class(my_character) #Output will be "character"
+class(my_logical) #Output will be "logical" 
+
+

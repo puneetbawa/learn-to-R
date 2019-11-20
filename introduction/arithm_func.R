85b1941285e431c9edc5eaa22a80f95e3d80f405
Puneet Bawa <bawapuneet994@gmail.com>
2019-11-20 15:01:36 +0530

    Adding random function, arithematic function and letters character

---
+++ introduction/arithm_func.R
@@ -0,0 +1,17 @@
+#Applying mean, median,variance, standard deviation and summary to the the vector function
+
+y<-runif(5,min=1,max=100)
+
+mean(y)
+median(y)
+var(y)
+sd(y)
+summary(y)
+
+y<-c("green","red","blue")
+summary(y)
+
+y<-c(23,50,70,23,20,23,"")
+summary(y)
+
+
---
+++ introduction/letters.R
@@ -0,0 +1,13 @@
+#code to generate letters 
+
+letters
+which(letters=="f")
+
+LETTERS
+which(letters=="C")
+
+LETTERS
+which(LETTERS=="C")
+
+letters
+which(letters=="C")
